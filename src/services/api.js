95d09114fd1da61bc015ba09e2f6a3a660d3c889
srotoswini <srotoswini.dev@gmail.com>
2022-06-12 14:15:33 +0530

    .

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import setSigns from "../App"
+import React from "react"
+
+// for zodiac signs
+export const getSigns = async () => {
+    const response = await fetch("http://sandipbgt.com/theastrologer/api/sunsigns/")
+  return await response.json()
+}
+
+// for horoscope
+export const getHoroscope = async (sign, time) => {
+    const response = await fetch(`http://sandipbgt.com/theastrologer/api/horoscope/${sign}/${time}`)
+  const { horoscope } = await response.json()
+  return horoscope
+}
