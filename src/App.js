3100110be1c79e07b29a40050eb06c3a5aaf91e9
srotoswini <srotoswini.dev@gmail.com>
2022-06-12 14:12:44 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 // eslint-disable
 import React, { useState } from "react";
+import { SelectSign } from "./components/SelectSign";
+import { SelectTime } from "./components/SelectTime";
+import { Horoscope } from "./components/Horoscope";
 import './App.css';
 
 function App() {
+
+  const [selectedSign, setSelectedSign] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const restart = () => {
+    setSelectedSign(null);
+    setSelectedTime(null);
+  }
+
   return (
     <div className="App">
       <h1>Hello Hackers !</h1>
