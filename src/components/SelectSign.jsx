e4dda1d0aa3cd227f7cd880186d95d5c941545a7
srotoswini <srotoswini.dev@gmail.com>
2022-06-12 14:17:37 +0530

    .

--- src/components/SelectSign.jsx
+++ src/components/SelectSign.jsx
@@ -1,2 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { getSigns } from "../services/api";
+
+export const SelectSign = ({ setSelectedSign }) => {
+  const [signs, setSigns] = useState([]);
+
+  useEffect(() => {
+    getSigns().then(setSigns);
+  });
+  return (
+    <>
+      <h2>Please select your zodiac sign</h2>
+      <div className="grid">
+        {signs.map((sign) => (
+          <button
+            className="sign"
+            key={sign}
+            onClick={() => setSelectedSign(sign)}
+          >
+            {sign}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
