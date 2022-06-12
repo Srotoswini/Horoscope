05f5fb571ca44edd3d0084fafc24266608d057b2
srotoswini <srotoswini.dev@gmail.com>
2022-06-12 13:51:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+// eslint-disable
 import React, { useState } from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <h1>Hello Hackers !</h1>
     </div>
   );
 }
