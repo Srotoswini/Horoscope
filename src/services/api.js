9d30f3396e5c75f2260397c5dbbd45b87fe622ea
srotoswini <srotoswini.dev@gmail.com>
2022-06-12 15:55:06 +0530

    .

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ export const getSigns = async () => {
 
 // for horoscope
 export const getHoroscope = async (sign, time) => {
-    const response = await fetch(`http://sandipbgt.com/theastrologer/api/horoscope/${sign}/${time}`)
-  const { horoscope } = await response.json()
-  return horoscope
+const response = await fetch(`http://sandipbgt.com/theastrologer/api/horoscope/${sign}/${time}`)
+const { horoscope } = await response.json()
+return horoscope
 }
